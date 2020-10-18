419c6274b1eae70615747f3af8206f841aa1efa7
Petr Sicho <xsicp01@gjk.cz>
2020-10-18 20:00:08 +0200

    swing animation & general weapon template

--- Scripts/Hands.gd
+++ Scripts/Hands.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+onready var animation_tree = get_node("AnimationTree")
+onready var animation_mode = animation_tree["parameters/playback"]
+
+var active_weapon: Node
+
 func _mouse_angle():
 	var x = get_viewport().get_mouse_position().x - get_viewport_rect().size.x/2
 	var y = get_viewport().get_mouse_position().y - get_viewport_rect().size.y/2
@@ -12,21 +17,47 @@ func _mouse_angle():
 		angle = PI - angle
 	angle = angle * (1 if y < 0 else -1)
 	return angle/PI*180
-	
+
 func look_at_mouse():
 	var angle = _mouse_angle()
-	if $Right/Weapon.flip_h:
-		$Right/Weapon.frame_coords.x = round((sign(angle)*180 - angle + 100) / 10)
+	if active_weapon.flip_h:
+		active_weapon.frame_coords.y = round((sign(angle)*180 - angle + 100) / 10)
 	else:
-		$Right/Weapon.frame_coords.x = round((angle + 100) / 10)
+		active_weapon.frame_coords.y = round((angle + 100) / 10)
 	if angle > 105 or angle < -105:
-		$Right/Weapon.flip_h = true
+		active_weapon.flip_h = true
 	elif angle < 75 and angle >= 0 or angle > -75 and angle <= 0:
-		$Right/Weapon.flip_h = false
+		active_weapon.flip_h = false
 
+func _input(event):
+	if event.is_action_pressed("attack"):
+		active_weapon.left_click(animation_mode)
 
 func _ready():
-	$AnimationPlayer.current_animation = "Idle"
-	
+	active_weapon = $Weapons/Cutlass
+	active_weapon.idle(animation_mode)
+
 func _process(delta):
 	look_at_mouse()
+	
+class_name Weapon
+
+class Melee extends Sprite:
+	var data = {
+		idle_animation = "",
+		left_click_animation = "",
+		right_click_animation = "",
+		damage = 0,
+		timeout = 0
+	}
+		
+	func left_click(animation_mode):
+		animation_mode.travel(data.left_click_animation)
+		print(data.left_click_animation)
+
+	func right_click(animation_mode):
+		animation_mode.travel(data.right_click_animation)
+
+	func idle(animation_mode):
+		animation_mode.start(data.idle_animation)
+
---
+++ Scripts/tileset_binder.gd
@@ -0,0 +1,13 @@
+extends TileSet
+tool
+
+var road_no_border = find_tile_by_name("road_no_border")
+var road_border = find_tile_by_name("road_border")
+var grass_no_border = find_tile_by_name("grass_no_border")
+var grass_border = find_tile_by_name("grass_border")
+
+func _is_tile_bound(drawn_id, neighbor_id):
+	return drawn_id == grass_no_border and neighbor_id == grass_border or \
+		drawn_id == road_no_border and neighbor_id == road_border or \
+		drawn_id == grass_no_border and neighbor_id == road_border or \
+		drawn_id == road_no_border and neighbor_id == grass_border
