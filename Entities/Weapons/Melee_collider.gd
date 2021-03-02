7f9f70be5dec210e6107c8dd5818bd9431e39ba4
lesves <lukas.veskrna@gmail.com>
2021-03-02 11:39:47 +0100

    fixes

---
+++ Entities/Weapons/Melee_collider.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+#will register all objects inside collider
+var inside_click_range = []
+
+func _ready():
+	var success = false #connect to events
+	success = success or connect("area_entered", self, "_on_area_entered")
+	success = success or connect("area_exited", self, "_on_area_exited")
+	assert(!success, "Collider detector failed to connect!")
+
+func _on_area_entered(area):
+	inside_click_range.append(area)
+
+func _on_area_exited(area):
+	inside_click_range.remove(inside_click_range.bsearch(area))
+
+func inside():
+	return inside_click_range
